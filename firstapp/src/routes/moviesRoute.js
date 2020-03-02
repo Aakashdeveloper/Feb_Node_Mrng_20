38622357bc8776a6f478ab250ce7a6af807fde05
aakashDeveloper <ahanda205@gmail.com>
2020-03-02 01:12:33 +0000

    a

--- firstapp/src/routes/moviesRoute.js
+++ firstapp/src/routes/moviesRoute.js
@@ -1,43 +1,47 @@
 var express = require('express');
 var moviesRouter = express.Router();
+var mongodb = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017";
 
-var movies =[
-    {
-        "_id": "5ab12612f36d2879268f284a",
-        "name": "Black Panther",
-        "language": "ENGLISH",
-        "rate": 4.5,
-        "type": "Action Adventure Fantasy",
-        "imageUrl": "https://image.ibb.co/f0hhZc/bp.jpg"
-      },
-      {
-        "_id": "5ab12666f36d2879268f2902",
-        "name": "Death Wish",
-        "language": "ENGLISH",
-        "type": "Action Crime Thriller",
-        "rate": 3.2,
-        "imageUrl": "https://image.ibb.co/gC9PfH/dw.jpg"
-      },
-      {
-        "_id": "5ab12678f36d2879268f291d",
-        "name": "Coco",
-        "language": "ENGLISH",
-        "type": "Adventure Animation Family",
-        "rate": 5,
-        "imageUrl": "https://image.ibb.co/dQwWSx/coco.jpg"
-      }
-]
 
 function router(menu){
   moviesRouter.route('/')
     .get(function(req,res){
-      //res.send(movies)
-      res.render('movies',{title:'Movies Page',navigation:menu,movies:movies})
+      mongodb.connect(url,function(err,dc){
+        if(err){
+          res.status(501).send('Error while connecting')
+        }else{
+          const dbo = dc.db('classdatabase');
+          dbo.collection('movies').find({}).toArray((err,data) => {
+            if(err){
+              res.status(402).send('Error while fetching data')
+            }else{
+              res.render('movies',{title:'Movies Page',navigation:menu,movies:data})
+            }
+          })
+        }
+      })
+      
+      
   });
 
-  moviesRouter.route('/details')
+  moviesRouter.route('/details/:id')
     .get(function(req,res){
-      res.send('Details of movies')
+      var id = req.params.id;
+      mongodb.connect(url,function(err,dc){
+        if(err){
+          res.status(501).send('Error while connecting')
+        }else{
+          const dbo = dc.db('classdatabase');
+          dbo.collection('movies').findOne({_id:id},(err,data) => {
+            if(err){
+              res.status(402).send('Error while fetching data')
+            }else{
+              res.render('moviesDetails',{title:'Movies Details',navigation:menu,movies:data})
+            }
+          })
+        }
+      })
   });
 
   return moviesRouter
